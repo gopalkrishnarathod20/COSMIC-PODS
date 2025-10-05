7a063ca8304facb662c94dc637687698cff31f97
RATHOD GOPALKRISHNA BHIMRAO <rathodgopalkrishnabhimrao@GOPALKRISHNAs-MacBook-Air.local>
2025-10-05 09:45:49 +0100

    Final Commit

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -6,7 +6,7 @@ import { TiLocationArrow } from "react-icons/ti";
 
 import Button from "./Button";
 
-const navItems = ["About", "Contact"];
+const navItems = [];
 
 const NavBar = () => {
   const [isAudioPlaying, setIsAudioPlaying] = useState(false);
